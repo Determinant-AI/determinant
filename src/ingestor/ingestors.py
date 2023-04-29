0933ff336b8532bba4f3caa11d9dce896f2b9699
Fan Pan <fanpan2022@gmail.com>
2023-04-28 21:30:06 -0700

    add data class

---
+++ src/ingestor/ingestors.py
@@ -0,0 +1,18 @@
+class Ingestor(object):
+    def __init__(self):
+        pass
+
+# KnowledgeIngestor ingests knowledge from relatively "static" sources e.g. confluence, wiki, etc.
+# On demand or on a schedule in the background.
+
+
+class KnowledgeIngestor(Ingestor):
+    def __init__(self):
+        pass
+
+# ChatIngestor ingests the chat messages from Slack, Teams, etc, on real time.
+
+
+class ChatIngestor(Ingestor):
+    def __init__(self):
+        pass
